aaa0baa65589ebac50106579650e8fe820337ec0
Daria Vorobiova <91011906+hero8721@users.noreply.github.com>
2022-09-07 12:23:42 +0200

    Create palindrome.js

---
+++ palindrome.js
@@ -0,0 +1,17 @@
+const prom = prompt('Write a word: ');
+const prom_1 = prom.split("").reverse();
+
+let i = 0;
+
+while(i<prom.length){
+    if(prom[i] == prom_1[i]){
+        i++;
+    }
+    else{
+        console.log('Not palindrome');
+        break;
+    }  
+}
+if(i==prom.length){
+    console.log(`This word is palindrome: ${prom}`);
+}
